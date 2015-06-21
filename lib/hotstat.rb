94c9720509b62688f504836ac896f9860d5a5414
Buddy Magsipoc <keikun17@gmail.com>
2015-06-21 19:50:30 +0800

    Pass。

--- lib/hotstat.rb
+++ lib/hotstat.rb
@@ -1,5 +1,46 @@
 require "hotstat/version"
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 
 module Hotstat
-  # Your code goes here...
+
+  PLAYER_URL = 'http://hosmax.com/player/detail/?name='
+
+  def self.fetch_data(players_arr)
+    players = []
+
+    players = players_arr.map do |player_code|
+      player_str = player_code.gsub('#', '%23')
+      raw_html = open(PLAYER_URL + player_str)
+      page = Nokogiri::HTML(raw_html)
+
+      player_name = page.css('[class=hero-title]').first.children.first.content
+
+      # Remove whitespace on the fetched name
+      player_name.gsub!(' ', '')
+
+      # Remove endline on fetched name
+      player_name.gsub!("\n","")
+
+      # Fetch mmrs
+      mmrs = page.css('[class=mmr]')
+
+
+      if mmrs[0]
+        mmr1 = mmrs[0].content
+      end
+
+      if mmrs[1]
+        mmr2 = mmrs[1].content
+      end
+
+      # Instantiate player
+      Player.new(name: player_name , mmr1: mmr1, mmr2: mmr2)
+    end
+
+    players
+  end
+
+
 end
--- spec/lib/hotstat_spec.rb
+++ spec/lib/hotstat_spec.rb
@@ -9,10 +9,10 @@ describe Hotstat do
     it "returns player data" do
       player_list = ['Arkanthiel#1571', 'PlayerOne#1309']
 
-      expect(described_class.fetch_data(player_list)).to include([
+      expect(described_class.fetch_data(player_list)).to include(
         an_object_having_attributes(name: 'Arkanthiel#1571', mmr1: '1475', mmr2: '1452'),
-        an_object_having_attributes(name: 'PlayerOne#1309')
-      ])
+        an_object_having_attributes(name: 'PlayerOne#1309', mmr1: '1472')
+      )
     end
 
   end
